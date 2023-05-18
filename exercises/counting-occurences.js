7914d0260228f6e6b9c27b92768caec7670b3ef8
Ahbideen Yusuf <ahbideenyusuf@gmail.com>
2023-05-18 09:34:32 +0100

    Adding a new exercise

---
+++ exercises/counting-occurences.js
@@ -0,0 +1,31 @@
+// [1, 1, 2, 2, 2, 3]
+
+var solution = function (array) {
+  let obj = {};
+
+  for (let item of array) {
+    if (obj[item] === undefined) {
+      obj[item] = 1;
+    } else {
+      obj[item] += 1;
+    }
+  }
+
+  let newArr = [];
+
+  for (let element in obj) {
+    newArr.push({ key: element, value: obj[element] });
+  }
+
+  let sorted = newArr.sort((a, b) => {
+    if (a.value > b.value) return 1;
+    else if (a.value < b.value) return -1;
+    return 0;
+  });
+
+  return parseInt(sorted[0].key);
+};
+
+console.log(solution([1, 1, 2, 2, 2, 3]));
+
+("");
